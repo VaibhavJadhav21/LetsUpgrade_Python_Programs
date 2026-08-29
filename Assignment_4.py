290b7707f1f39ed2d846f0e155a6aac60b92cd14
VaibhavJadhav21 <75692519+VaibhavJadhav21@users.noreply.github.com>
2026-08-29 15:46:15 +0530

    Update Assignment_4.py

--- Assignment_4.py
+++ Assignment_4.py
@@ -1,34 +1,46 @@
-# write down a program for opening a file and writing "I Love Letsupgrade" and closeit, and read it back again,and then append some data to it and close it
-# answer
-f = open("temp.txt", "w")
-f.write("I Love Letsupgrade!")
-f.close()
+@Test
+void generateReport_ShouldBuildTransactionReport() {
+    UUID reportManagementId = UUID.randomUUID();
 
-f = open("temp.txt", "r")
-print(f.read())
+    ReportManagementDto dto = new ReportManagementDto();
+    dto.setId(reportManagementId);
+    dto.setReport(ReportType.TRANSACTION);
 
-f = open("temp.txt", "a")
-f.write("Now the file has more content!")
-f.close()
+    when(reportManagementDao.updateReportStatus(
+            reportManagementId, ReportStatus.GENERATION_STARTED))
+            .thenReturn(dto);
+
+    when(reportDao.getTransactionReportData(dto))
+            .thenReturn(transactionReportData);
+
+    reportService.generateReport(reportManagementId);
+
+    verify(reportDao).getTransactionReportData(dto);
+    verify(reportManagementDao).updateReportStatus(
+            reportManagementId, ReportStatus.GENERATION_STARTED);
+}
 
 
-# Write a Function Which can Return a Factorial of any number as int,given in the argument
-# answer
 
-# change the value for a different result
-num = 7
 
-# To take input from the user
-#num = int(input("Enter a number: "))
+@Test
+void generateReport_ShouldBuildSettlementReport() {
+    UUID reportManagementId = UUID.randomUUID();
 
-factorial = 1
+    ReportManagementDto dto = new ReportManagementDto();
+    dto.setId(reportManagementId);
+    dto.setReport(ReportType.SETTLEMENT);
 
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+    when(reportManagementDao.updateReportStatus(
+            reportManagementId, ReportStatus.GENERATION_STARTED))
+            .thenReturn(dto);
+
+    when(reportDao.getSettlementReportData(dto))
+            .thenReturn(settlementReportData);
+
+    reportService.generateReport(reportManagementId);
+
+    verify(reportDao).getSettlementReportData(dto);
+    verify(reportManagementDao).updateReportStatus(
+            reportManagementId, ReportStatus.GENERATION_STARTED);
+}
